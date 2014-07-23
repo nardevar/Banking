62bdd5eea272faa59a91d1421fa40efb28b8cd4b
nardevar <nardevar@in.ibm.com>
2014-07-23 16:23:51 +0530

    Modified

--- src/test/java/bankapp/database/BankDBTest.java
+++ src/test/java/bankapp/database/BankDBTest.java
@@ -14,21 +14,21 @@ import bankapp.database.BankDB;
 
 public class BankDBTest{
 	private static final long serialVersionUID = 1L;
-	
+
 	private static BankDB bankDB; 
-		
+
 	@BeforeClass
 	public static void setUp() throws Exception{
 		System.out.println("==>> inside setup");
 		bankDB = new BankDB();
 	}
-	
+
 	@AfterClass
 	public static void tearDown() throws Exception{
 		System.out.println("==>> inside tearDown");
 		bankDB.remove();
 	}
-	
+
 	@Test
 	public void testCustomerExist() throws Exception{
 		System.out.println("==>> inside testCustomerExist");
@@ -39,13 +39,13 @@ public class BankDBTest{
 		String lastName = "Mansour";
 
 		AuthenticationDetails authDetails = bankDB.customerExist(custId, pwd);
-		
+
 		System.out.println("==>> user id = "+authDetails.getUserID());
 		System.out.println("==>> pwd = "+authDetails.getPassword());
 		System.out.println("==>> account type = "+authDetails.getType());
 		System.out.println("==>> first name = "+authDetails.getFirstName());
 		System.out.println("==>> last name = "+authDetails.getLastName());
-		
+
 		Assert.assertNotNull(authDetails);
 		Assert.assertEquals(authDetails.getUserID(), custId);
 		Assert.assertEquals(authDetails.getType(), type);
@@ -63,19 +63,19 @@ public class BankDBTest{
 		String lastName = "Kiringa";
 
 		AuthenticationDetails authDetails = bankDB.employeeExist(empId, pwd);
-		
+
 		System.out.println("==>> user id = "+authDetails.getUserID());
 		System.out.println("==>> pwd = "+authDetails.getPassword());
 		System.out.println("==>> account type = "+authDetails.getType());
 		System.out.println("==>> first name = "+authDetails.getFirstName());
 		System.out.println("==>> last name = "+authDetails.getLastName());
-		
+
 		Assert.assertNotNull(authDetails);
 		Assert.assertEquals(authDetails.getUserID(), empId);
 		Assert.assertEquals(authDetails.getType(), type);
 		Assert.assertEquals(authDetails.getFirstName(), firstName);
 		Assert.assertEquals(authDetails.getLastName(), lastName);
-			
+
 	}
 
 	@Test
@@ -88,23 +88,26 @@ public class BankDBTest{
 		String firstName = "Mohamed";
 		String lastName = "Mansour";
 
-		CustomerDetails custDetails = bankDB.retrieveCustomerInformation(custId);
-		
-		System.out.println("==>> user id = "+custDetails.getCustomerId());
-		System.out.println("==>> pwd = "+custDetails.getCity());
-		System.out.println("==>> account type = "+custDetails.getEmail());
-		System.out.println("==>> first name = "+custDetails.getFirstName());
-		System.out.println("==>> last name = "+custDetails.getLastName());
-		
-		Assert.assertNotNull(custDetails);
-		Assert.assertEquals(custDetails.getCustomerId(), custId);
-		Assert.assertEquals(custDetails.getCity(), city);
-		Assert.assertEquals(custDetails.getEmail(), email);
-		Assert.assertEquals(custDetails.getFirstName(), firstName);
-		Assert.assertEquals(custDetails.getLastName(), lastName);
+		if(bankDB!=null){
+			CustomerDetails custDetails = bankDB.retrieveCustomerInformation(custId);
+
+			System.out.println("==>> user id = "+custDetails.getCustomerId());
+			System.out.println("==>> pwd = "+custDetails.getCity());
+			System.out.println("==>> account type = "+custDetails.getEmail());
+			System.out.println("==>> first name = "+custDetails.getFirstName());
+			System.out.println("==>> last name = "+custDetails.getLastName());
+
+			Assert.assertNotNull(custDetails);
+			Assert.assertEquals(custDetails.getCustomerId(), custId);
+			Assert.assertEquals(custDetails.getCity(), city);
+			Assert.assertEquals(custDetails.getEmail(), email);
+			Assert.assertEquals(custDetails.getFirstName(), firstName);
+			Assert.assertEquals(custDetails.getLastName(), lastName);
+		}else
+			System.out.println("bankDB is null");
 	}
 
 
 
-	
+
 }
